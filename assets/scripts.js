3e85cba50660dac2a2682da5e4619595a2aa8482
SwastikTripathi <88632085+SwastikTripathi@users.noreply.github.com>
2021-12-01 19:41:49 +0530

    Update scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -16,6 +16,16 @@ $(document).ready(function(){
             }
         }
     });
+    
+    $(document).ready(function() {
+        //Preloader
+        preloaderFadeOutTime = 500;
+        function hidePreloader() {
+            var preloader = $('#pre-loader');
+            preloader.fadeOut(preloaderFadeOutTime);
+        }
+        hidePreloader();
+    });
 
 
 });
