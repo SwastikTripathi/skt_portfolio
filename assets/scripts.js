9ca7975fcc47376576e4aa1dfe668a47674ea856
SwastikTripathi <88632085+SwastikTripathi@users.noreply.github.com>
2021-12-01 20:31:28 +0530

    Update scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -22,6 +22,7 @@ $(document).ready(function(){
     function hidePreloader() {
         var preloader = $('#pre-loader');
         preloader.fadeOut(preloaderFadeOutTime);
+        $('.after-load').removeClass('d-none');
     }
     hidePreloader();
     
